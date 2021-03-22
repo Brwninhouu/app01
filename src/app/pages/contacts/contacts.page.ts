1c22d0b90e1e3016258b7eacab411b8fe7c632ef
Bruno Capella <bru_o_gato@hotmail.com>
2021-03-22 08:33:10 -0300

    ultima aula

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,15 +1,133 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) Importa dependências
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { DatePipe } from '@angular/common';
+import { AlertController } from '@ionic/angular';
+
+// 6) Não permite somente espaços nos campos
+export function removeSpaces(control: AbstractControl) {
+  if (control && control.value && !control.value.replace(/\s/g, '').length) {
+    control.setValue('');
+  }
+  return null;
+}
+
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.page.html',
   styleUrls: ['./contacts.page.scss'],
 })
 export class ContactsPage implements OnInit {
+  // 3) Atributos
+  public contactForm: FormGroup; // Contém o formulário de contatos
+  public pipe = new DatePipe('en_US'); // Formatar as datas
 
-  constructor() { }
+  constructor(
+    // 2) Injeta dependências
+    public form: FormBuilder,
+    public firestore: AngularFirestore,
+    public alert: AlertController
+
+  ) { }
 
   ngOnInit() {
+    // 4) Cria o formulário de contatos
+    this.contactFormCreate();
   }
 
+  // 5) Cria o formulário de contatos
+  contactFormCreate() {
+    this.contactForm = this.form.group({
+      date: [''], // Data de envio
+      name: [
+        // Nome
+        '',
+        Validators.compose([
+          Validators.required, // Obrigatório
+          Validators.minLength(3), // Nomes curtos demais
+          removeSpaces
+        ]),
+      ],
+      email: [
+        // E-mail
+        '',
+        Validators.compose([Validators.required, Validators.email, removeSpaces]),
+      ],
+      subject: [
+        // Assunto
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+      ],
+      message: [
+        // Mensagem
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5), removeSpaces]),
+      ],
+    });
+  }
+
+
+
+  //7) processa o envio do formulário
+  contactSend() {
+
+    //criar e formartar a data
+    this.contactForm.controls.date.setValue(
+      this.pipe.transform(Date.now(), 'yyyy-MM-dd HH:mm:ss').trim()
+    );
+
+
+    // salva em um novo documento do firebase Firestore
+    this.firestore.collection('contacts').add(this.contactForm.value)
+      .then(
+        () => {
+
+          //reset do formulário
+          this.contactForm.reset();
+
+          // feedback
+
+          this.presentAlert();
+
+
+
+          alert('Contato enviado com sucesso!');
+        }
+      )
+      .catch(
+        (error) => {
+
+          alert('Algo de errado não está certo' + error);
+
+        }
+
+      );
+
+  }
+
+
+  //feedback
+  async presentAlert() {
+    const alert = await this.alert.create({
+
+      header: 'Oba!',
+      message: 'Contato enviado com sucesso!',
+      buttons: [{
+        text: 'Beleza!',
+        handler: () => {
+
+          this.contactForm.reset();
+        }
+      }]
+    });
+
+    await alert.present();
+  }
 }
--- src/app/pages/news/news.module.ts
+++ src/app/pages/news/news.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { NewsPageRoutingModule } from './news-routing.module';
-
 import { NewsPage } from './news.page';
-
+// importa http client do angular
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NewsPageRoutingModule
+    NewsPageRoutingModule,
+    //declara módulo declarado
+    HttpClientModule 
   ],
   declarations: [NewsPage]
 })
